c90eff3a7b272762b6e782c0b76a93bb7e03f49d
skuethe <crazycapivara@gmail.com>
2017-03-10 20:13:59 +0100

    Update install script

--- rosgeo-live_install.sh
+++ rosgeo-live_install.sh
@@ -1,10 +1,17 @@
-# R packages
-sudo apt-get install -y libudunits2-dev libv8-dev
+#!/bin/bash
+arch=$1
 
-# RStudio
-rstudio=rstudio-1.0.136-i386.deb
+# --- R packages
+#sudo apt-get install -y libudunits2-dev libv8-dev
+
+# --- RStudio
+rstudio=rstudio-1.0.136-amd64.deb
+if [ "$arch" == "i386" ]; then
+	rstudio=rstudio-1.0.136-i386.deb
+fi
+echo $rstudio
 #wget https://download1.rstudio.org/$rstudio
 #dpkg -I $rstudio
-sudo apt-get install -y libjpeg62 libedit2 libgstreamer0.10-0 libgstreamer-plugins-base0.10-0 libssl1.0.0
-sudo dpkg -i $rstudio
+#sudo apt-get install -y libjpeg62 libedit2 libgstreamer0.10-0 libgstreamer-plugins-base0.10-0 libssl1.0.0
+#sudo dpkg -i $rstudio
 
